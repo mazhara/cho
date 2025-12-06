0d6daf1dd51acef14dad9dce4746c6b8e34b2722
Dmytro Aleksandrov <alkersan@gmail.com>
2025-12-06 17:01:23 -0500

    Make root API url relative to window address during production build

--- app/src/main/scala/toloka/cho/books/common/Constants.scala
+++ app/src/main/scala/toloka/cho/books/common/Constants.scala
@@ -1,7 +1,8 @@
 package toloka.cho.books.common
 
-import scala.scalajs.js
+import scala.scalajs.{LinkingInfo, js}
 import scala.scalajs.js.annotation.JSImport
+import org.scalajs.dom.window
 
 object Constants {
   @js.native
@@ -15,9 +16,11 @@ object Constants {
   val defaultPageSize = 20
 
   object endpoints {
-    val root = "http://localhost:4041"
+    val root =
+      if (LinkingInfo.developmentMode) "http://localhost:4041"
+      else window.location.origin
+
     val books = s"$root/api/books"
     val getFilters = s"$root/api/books/filters"
   }
 }
-
